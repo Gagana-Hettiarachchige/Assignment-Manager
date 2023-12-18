3331524aedb95527f8235432a36024cf4a059400
Gagana Hettiarachchige <81533753+Gagana-Hettiarachchige@users.noreply.github.com>
2023-12-17 20:36:42 -0500

    Started database.

---
+++ AMDB.sql
@@ -0,0 +1,24 @@
+/* 
+* FILE          : AssignmentDatabase.sql
+* PROJECT       : Assignment Manager
+* PROGRAMMER    : Gagana Hettiarachchige
+* FIRST VERSION : 2023-12-17
+* DESCRIPTION   :
+*	This file holds the assignments database.
+*/
+
+DROP DATABASE IF EXISTS AM;
+CREATE DATABASE AM;
+USE AM;
+
+
+/* Holds assigment details. */
+CREATE TABLE Assignments
+(
+    AssignmentNumber INT NOT NULL AUTO_INCREMENT,
+    ClassName VARCHAR(30) NOT NULL,
+    AssignmentName VARCHAR(30) NOT NULL,
+    CourseWeight FLOAT(10) NOT NULL,
+    DueDate DATETIME NOT NULL,
+    PRIMARY KEY (AssignmentNumber)
+);
