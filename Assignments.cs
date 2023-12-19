71f6fc9656827eb3081cb6eaf5f1bf0a6fc0386e
Gagana Hettiarachchige <81533753+Gagana-Hettiarachchige@users.noreply.github.com>
2023-12-19 17:21:44 -0500

    Added properties to represents an assignment row in the database.

--- Assignments.cs
+++ Assignments.cs
@@ -10,6 +10,7 @@
 
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,8 +18,20 @@ using System.Windows.Controls;
 
 namespace AssignmentManager.CodeFiles
 {
-    internal class Assignments
+    internal class Assignment
     {
-        
+        /* Assignment table columns. */
+        public int AssignmentNumber { get; set; }
+        public string ClassName { get; set; }
+        public string AssignmentName { get; set; }
+        public double AssignmentWeight { get; set; }
+        public DateTime DueDate { get; set; }
+        public string AssignmentStatus { get; set; }
+        public string LocalResources { get; set; }
+        public string OnlineResources { get; set; }
+        public string GitFolder { get; set; }
+
+
+
     }
 }
