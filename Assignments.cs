1b619c3bc2dee1474626f6c3b412f8d98c4e94ac
Gagana Hettiarachchige <81533753+Gagana-Hettiarachchige@users.noreply.github.com>
2023-12-18 20:52:56 -0500

    Added file header comment.

---
+++ Assignments.cs
@@ -0,0 +1,24 @@
+﻿/* 
+* FILE          : Assignments.cs
+* PROJECT       : Assignment Manager
+* PROGRAMMER    : Gagana Hettiarachchige
+* FIRST VERSION : 2023-12-18
+* DESCRIPTION   :
+*	Holds the definiton of a class that is to be
+*	used as values in the data grid.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace AssignmentManager.CodeFiles
+{
+    internal class Assignments
+    {
+        
+    }
+}
