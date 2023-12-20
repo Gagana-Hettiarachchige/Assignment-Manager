8c90680deb48f2061d4c75af62839dd75f2f087f
Gagana Hettiarachchige <81533753+Gagana-Hettiarachchige@users.noreply.github.com>
2023-12-20 18:22:41 -0500

    Added file header comment.

---
+++ ModifyOnlineResources.xaml.cs
@@ -0,0 +1,37 @@
+﻿/* 
+* FILE          : ModifyOnlineResources.xaml.cs
+* PROJECT       : Assignment Manager
+* PROGRAMMER    : Gagana Hettiarachchige
+* FIRST VERSION : 2023-12-20
+* DESCRIPTION   :
+*	This file is the code behind for the menu to
+*	modify the online resources.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace AssignmentManager.CodeFiles
+{
+    /// <summary>
+    /// Interaction logic for ModifyOnlineResources.xaml
+    /// </summary>
+    public partial class ModifyOnlineResources : Window
+    {
+        public ModifyOnlineResources()
+        {
+            InitializeComponent();
+        }
+    }
+}
