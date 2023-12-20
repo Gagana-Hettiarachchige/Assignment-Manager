905f65fa49e4ce8c97c827a8fd3a0157847fb624
Gagana Hettiarachchige <81533753+Gagana-Hettiarachchige@users.noreply.github.com>
2023-12-20 07:24:26 -0500

    Made actual data members with defaults.

--- Assignments.cs
+++ Assignments.cs
@@ -20,16 +20,63 @@ namespace AssignmentManager.CodeFiles
 {
     internal class Assignment
     {
-        /* Assignment table columns. */
-        public int AssignmentNumber { get; set; }
-        public string ClassName { get; set; }
-        public string AssignmentName { get; set; }
-        public double AssignmentWeight { get; set; }
-        public DateTime DueDate { get; set; }
-        public string AssignmentStatus { get; set; }
-        public string LocalResources { get; set; }
-        public string OnlineResources { get; set; }
-        public string GitFolder { get; set; }
+        /* Assignment table column values. */
+        private int assignmentNumber = 0;
+        private string className = "";
+        private string assignmentName = "";
+        private double assignmentWeight = 0.0;
+        private DateTime dueDate = DateTime.Now;
+        private string assignmentStatus = "";
+        private string localResources = "";
+        private string onlineResources = "";
+        private string gitFolder = "";
+
+        /* Properties for assignment assignment values. */
+        public int AssignmentNumber 
+        { 
+            get { return assignmentNumber; } 
+            set { assignmentNumber = value; } 
+        }
+        public string ClassName 
+        {
+            get { return className; }
+            set { className = value; }
+        }
+        public string AssignmentName 
+        {
+            get { return assignmentName; }
+            set { assignmentName = value; }
+        }
+        public double AssignmentWeight 
+        {
+            get { return assignmentWeight; }
+            set { assignmentWeight = value; }
+        }
+        public DateTime DueDate 
+        {
+            get { return dueDate; }
+            set { dueDate = value; }
+        }
+        public string AssignmentStatus 
+        {
+            get { return assignmentStatus; }
+            set { assignmentStatus = value; }
+        }
+        public string LocalResources 
+        {
+            get { return localResources; }
+            set { localResources = value; }
+        }
+        public string OnlineResources 
+        {
+            get { return onlineResources; }
+            set { onlineResources = value; }
+        }
+        public string GitFolder 
+        {
+            get { return gitFolder; }
+            set { gitFolder = value; }
+        }
 
 
 
