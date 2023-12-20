15e611735b65bfd4ab1d0650f7d8dd12f465b60a
Gagana Hettiarachchige <81533753+Gagana-Hettiarachchige@users.noreply.github.com>
2023-12-20 06:51:06 -0500

    Added file header comment.

---
+++ ViewModel.cs
@@ -0,0 +1,23 @@
+﻿/* 
+* FILE          : ViewModel.cs
+* PROJECT       : Assignment Manager
+* PROGRAMMER    : Gagana Hettiarachchige
+* FIRST VERSION : 2023-12-20
+* DESCRIPTION   :
+*	This file acts as a view model for the
+*	selected assignment, edited assignment, and collections
+*	of items.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssignmentManager.CodeFiles
+{
+    internal class ViewModel
+    {
+    }
+}
