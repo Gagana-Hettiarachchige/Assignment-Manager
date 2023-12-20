54d14bb36a0852f3ecf672ec9d8a027e13a0a21d
Gagana Hettiarachchige <81533753+Gagana-Hettiarachchige@users.noreply.github.com>
2023-12-19 20:06:54 -0500

    Added documention.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -33,16 +33,32 @@ namespace AssignmentManager
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    
+
+    /*
+    * NAME	  : MainWindow
+    * PURPOSE : The main window allows for the user to interact
+    *           with the assignments by inserting, altering, deleting,
+    *           and display assignments and information about them.
+    */
     public partial class MainWindow : Window
     {
 
         /* All assignments. */
         ObservableCollection<Assignment> loadedAssignments = new ObservableCollection<Assignment>();
-
-
         int assignmentNumber = 0;   // Testing substitution for assignment number.
 
 
+
+        /* Constructors. */
+
+        /* 
+        * METHOD        : MainWindow
+        * DESCRIPTION   :
+        *   Constructor that creates the main window
+        * PARAMETERS    :
+        *   void
+        */
         public MainWindow()
         {
             InitializeComponent();
@@ -58,6 +74,21 @@ namespace AssignmentManager
             StatusComboBox.SelectedIndex = 0;
         }
 
+
+
+        /* Event handlers. */
+
+        /* 
+        * METHOD        : SchoolButton_Click
+        * DESCRIPTION   :
+        *   Raised when the school button is clicked and
+        *   opens a new tab to the school's homepage.
+        * PARAMETERS    :
+        *   object sender   : the sender
+        *   RouterEventArgs : the routed event arguments
+        * RETURNS       :
+        *   void
+        */
         private void SchoolButton_Click(object sender, RoutedEventArgs e)
         {
             /* Opens tab to school's webpage. */
@@ -65,6 +96,18 @@ namespace AssignmentManager
         }
 
 
+        /* 
+        * METHOD        : LocalResourcesAddButton_Click
+        * DESCRIPTION   :
+        *   Raised when the ocal resources add file is clicked and
+        *   creates an open file dialog to select a resource and
+        *   saves it to the local resources combo box
+        * PARAMETERS    :
+        *   object sender   : the sender
+        *   RouterEventArgs : the routed event arguments
+        * RETURNS       :
+        *   void
+        */
         private void LocalResourcesAddButton_Click(object sender, RoutedEventArgs e)
         {
             /* Setting up dialog for adding files to be able to open. */
@@ -95,6 +138,19 @@ namespace AssignmentManager
             
         }
 
+
+        /* 
+        * METHOD        : LocalResourcesAddFolderButton_Click
+        * DESCRIPTION   :
+        *   Raised when the local resouces add folder button is clicked and
+        *   creates and open folder dialog so select a folder and
+        *   adds it to the local resource combo box.
+        * PARAMETERS    :
+        *   object sender   : the sender
+        *   RouterEventArgs : the routed event arguments
+        * RETURNS       :
+        *   type : description
+        */
         private void LocalResourcesAddFolderButton_Click(object sender, RoutedEventArgs e)
         {
             CommonOpenFileDialog resource_folder = new CommonOpenFileDialog();
@@ -110,12 +166,36 @@ namespace AssignmentManager
         }
 
 
+        /* 
+        * METHOD        : LocalResourcesRemoveButton_Click
+        * DESCRIPTION   :
+        *   Raised when the local resources remove button is clicked and
+        *   removes the selected resource in the local resouces combo box.
+        * PARAMETERS    :
+        *   object sender   : the sender
+        *   RouterEventArgs : the routed event arguments
+        * RETURNS       :
+        *   void
+        */
         private void LocalResourcesRemoveButton_Click(object sender, RoutedEventArgs e)
         {
             /* Removing selected resource from the combo box. */
             LocalResourcesComboBox.Items.Remove(LocalResourcesComboBox.SelectedItem);
         }
 
+
+        /* 
+        * METHOD        : OnlineResourcesAddButton_Click
+        * DESCRIPTION   :
+        *   Raised when the online resources add button is clicked and 
+        *   adds the online resource entered in the online resource texbox to
+        *   the online resouces combo box.
+        * PARAMETERS    :
+        *   object sender   : the sender
+        *   RouterEventArgs : the routed event arguments
+        * RETURNS       :
+        *   void
+        */
         private void OnlineResourcesAddButton_Click(object sender, RoutedEventArgs e)
         {
             /* Adding selected resource to the combo box. */
@@ -127,12 +207,37 @@ namespace AssignmentManager
             
         }
 
+
+        /* 
+        * METHOD        : OnlineResourcesRemoveButton_Click
+        * DESCRIPTION   :
+        *   Raised when the remove online resources button is clicked and
+        *   removes the select online resource from the combo box.
+        * PARAMETERS    :
+        *   object sender   : the sender
+        *   RouterEventArgs : the routed event arguments
+        * RETURNS       :
+        *   void
+        */
         private void OnlineResourcesRemoveButton_Click(object sender, RoutedEventArgs e)
         {
             /* Removing selected resource from the combo box. */
             OnlineResourcesComboBox.Items.Remove(OnlineResourcesComboBox.SelectedItem);
         }
 
+
+        /* 
+        * METHOD        : GitFolderAddButton_Click
+        * DESCRIPTION   :
+        *   Raised when the add git folder button is clicked and
+        *   creates a dialog to select a single folder and
+        *   adds it to the git folder combo box. 
+        * PARAMETERS    :
+        *   object sender   : the sender
+        *   RouterEventArgs : the routed event arguments
+        * RETURNS       :
+        *   void
+        */
         private void GitFolderAddButton_Click(object sender, RoutedEventArgs e)
         {
             
@@ -152,12 +257,37 @@ namespace AssignmentManager
             }
         }
 
+
+        /* 
+        * METHOD        : GitFolderRemoveButton_Click
+        * DESCRIPTION   :
+        *   Raised when the remove git folder button is clicked and
+        *   removes the selected folder from the git folder combo box.
+        * PARAMETERS    :
+        *   object sender   : the sender
+        *   RouterEventArgs : the routed event arguments
+        * RETURNS       :
+        *   void
+        */
         private void GitFolderRemoveButton_Click(object sender, RoutedEventArgs e)
         {
             /* Removing the folder. */
             SelectedGitFolderComboBox.Items.Clear();
         }
 
+
+        /* 
+        * METHOD        : InsertButton_Click
+        * DESCRIPTION   :
+        *   Raised when the insert button is clicked and
+        *   saves the new assignment in the database with
+        *   the details entered.
+        * PARAMETERS    :
+        *   object sender   : the sender
+        *   RouterEventArgs : the routed event arguments
+        * RETURNS       :
+        *   void
+        */
         private void InsertButton_Click(object sender, RoutedEventArgs e)
         {
             ++assignmentNumber; // Testing.
@@ -213,6 +343,19 @@ namespace AssignmentManager
             
         }
 
+
+        /* 
+        * METHOD        : Resources_MouseLeftButtonUp
+        * DESCRIPTION   :
+        *   Raised when either the local or online resources are clicked in
+        *   the grid and opens a menu to allow for opening the resources of that
+        *   assignment.
+        * PARAMETERS    :
+        *   object sender        : the sender
+        *   MouseButtonEventArgs : the mouse event args
+        * RETURNS       :
+        *   void
+        */
         private void Resources_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
         {
             /* Getting the resources. */
@@ -244,6 +387,17 @@ namespace AssignmentManager
         }
 
 
+        /* 
+        * METHOD        : Resources_MouseEnter
+        * DESCRIPTION   :
+        *   Raised when either the local or online resources are hovered over and
+        *   change the mouse to a hand icon.
+        * PARAMETERS    :
+        *   object sender        : the sender
+        *   MouseButtonEventArgs : the mouse event args
+        * RETURNS       :
+        *   void
+        */
         private void Resources_MouseEnter(object sender, MouseEventArgs e)
         {
             TextBlock text_block = (TextBlock)e.OriginalSource;
@@ -255,6 +409,18 @@ namespace AssignmentManager
             
         }
 
+
+        /* 
+        * METHOD        : Resources_MouseLeave
+        * DESCRIPTION   :
+        *   Raised when either the local or online resources are hovered off and
+        *   change the mouse back to an arrow.
+        * PARAMETERS    :
+        *   object sender        : the sender
+        *   MouseButtonEventArgs : the mouse event args
+        * RETURNS       :
+        *   void
+        */
         private void Resources_MouseLeave(object sender, MouseEventArgs e)
         {
             TextBlock text_block = (TextBlock)e.OriginalSource;
