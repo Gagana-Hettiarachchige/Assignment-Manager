f89a4d8bdbf5cfb9c3151af2fd290d8f8a65cba8
Gagana Hettiarachchige <81533753+Gagana-Hettiarachchige@users.noreply.github.com>
2023-12-20 06:49:34 -0500

    Added file header comment.

---
+++ DueDatePicker.xaml.cs
@@ -0,0 +1,36 @@
+﻿/* 
+* FILE          : DueDatePicker.xaml.cs
+* PROJECT       : Assignment Manager
+* PROGRAMMER    : Gagana Hettiarachchige
+* FIRST VERSION : 2023-12-20
+* DESCRIPTION   :
+*	Holds the code behind of the due date picker screen.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace AssignmentManager.CodeFiles
+{
+    /// <summary>
+    /// Interaction logic for DueDatePicker.xaml
+    /// </summary>
+    public partial class DueDatePicker : Window
+    {
+        public DueDatePicker()
+        {
+            InitializeComponent();
+        }
+    }
+}
