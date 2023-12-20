9b5159a0eab9b6ef52c4df17ad111f393230d367
Gagana Hettiarachchige <81533753+Gagana-Hettiarachchige@users.noreply.github.com>
2023-12-20 12:15:01 -0500

    Created logic to handle selecting, changing, and saving the due day and time.

--- DueDatePicker.xaml.cs
+++ DueDatePicker.xaml.cs
@@ -28,9 +28,156 @@ namespace AssignmentManager.CodeFiles
     /// </summary>
     public partial class DueDatePicker : Window
     {
+        private string dueDay = "";
+        private string dueHour = "";
+
+
+
+        /* Constructor. */
         public DueDatePicker()
         {
             InitializeComponent();
+            DueDayCalender.SelectedDate = DateTime.Now;
+        }
+
+        
+
+        /* Methods. */
+
+        /* 
+        * METHOD        : ChangeTime
+        * DESCRIPTION   :
+        *   Increments/decrements the time value by the
+        *   time change (negative or positibe) and keeps
+        *   the value inbetween a min and max.
+        * PARAMETERS    :
+        *   string time_value : the time value
+        *   int time_change   : the amount to change by (+/-)
+        *   int value_min     : the minimum the value can be
+        *   int value_max     : the maximum the value can be
+        * RETURNS       :
+        *   string : the updated time value
+        */
+        private string ChangeTime(string time_value, int time_change, 
+                                  int value_min, int value_max)
+        {
+            int hour = int.Parse(time_value);
+
+            /* Incrementing/decrementing the hour. */
+            hour += time_change;
+
+            /* Wrapping time around. */
+            if (hour > value_max)
+            {
+                hour = value_min;
+            }
+
+            else if (hour < value_min)
+            {
+                hour = value_max;
+            }
+
+
+            string hour_string;
+
+            /* Adding a leading 0 if single digit number. */
+            if (hour < 10)
+            {
+                hour_string = "0" + hour.ToString();
+            }
+            else
+            {
+                hour_string = hour.ToString();
+            }
+
+            return hour_string;
+        }
+
+
+        /* 
+        * METHOD        : 
+        * DESCRIPTION   :
+        *   Switches the AM/PM value.
+        * PARAMETERS    :
+        *   string current_value : current AM/PM
+        * RETURNS       :
+        *   string : the switched value
+        */
+        private string SwitchAmPm(string current_value)
+        {
+            /* Checking value and using opposite. */
+            if (current_value == "AM")
+            {
+                return "PM";
+            }
+
+            else if (current_value == "PM")
+            {
+                return "AM";
+            }
+
+            else
+            {
+                return "?";
+            }
+        }
+
+
+
+        /* Event handlers. */
+
+        private void Calendar_SelectedDatesChanged(object sender, SelectionChangedEventArgs e)
+        {
+            SelectedDatesCollection days = DueDayCalender.SelectedDates;
+
+            DateTime due_date_time = days[0];
+            dueDay = due_date_time.ToString("yyyy-MM-dd");
+        }
+
+        private void HourIncrementButton_Click(object sender, RoutedEventArgs e)
+        {
+            /* Incrementing and saving the hour. */
+            HourValue.Text = ChangeTime(HourValue.Text, 1, 1, 12);
+        }
+
+        private void HourDecrementButton_Click(object sender, RoutedEventArgs e)
+        {
+            /* Decrementing and saving the hour. */
+            HourValue.Text = ChangeTime(HourValue.Text, -1, 1, 12);
+        }
+
+        private void MinuteIncrementButton_Click(object sender, RoutedEventArgs e)
+        {
+            /* Incrementing and saving the minute. */
+            MinuteValue.Text = ChangeTime(MinuteValue.Text, 1, 0, 59);
+        }
+
+        private void MinuteDecrementButton_Click(object sender, RoutedEventArgs e)
+        {
+            /* Decrementing and saving the minute. */
+            MinuteValue.Text = ChangeTime(MinuteValue.Text, -1, 0, 59);
+        }
+
+        private void AmPmValueButton_Click(object sender, RoutedEventArgs e)
+        {
+            /* Switching AM to PM and PM to AM. */
+            AmPmValue.Text = SwitchAmPm(AmPmValue.Text);
+        }
+
+        private void SaveDueDateButton_Click(object sender, RoutedEventArgs e)
+        {
+            /* Getting the selected date */
+            SelectedDatesCollection days = DueDayCalender.SelectedDates;
+
+            /* Creating full date and time. */
+            dueDay = days[0].ToString("yyyy-MM-dd");
+            dueHour = HourValue.Text + ':' + MinuteValue.Text + ":00 " + AmPmValue.Text;
+
+            /* Saving in view model. */
+            ViewModel.SelectedAssignment.DueDate = DateTime.Parse(dueDay + ' ' + dueHour);
+
+            /* Closing the window. */
+            Close();
         }
     }
 }
