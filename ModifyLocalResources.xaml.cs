c3136f7f96bf31a98a496adabf4472c4126eabca
Gagana Hettiarachchige <81533753+Gagana-Hettiarachchige@users.noreply.github.com>
2023-12-20 14:45:44 -0500

    Added file header comment.

---
+++ ModifyLocalResources.xaml.cs
@@ -0,0 +1,37 @@
+﻿/* 
+* FILE          : ModifyLocalResources.xaml.cs
+* PROJECT       : Assignment Manager
+* PROGRAMMER    : Gagana Hettiarachchige
+* FIRST VERSION : 2023-12-20
+* DESCRIPTION   :
+*	This file holds the code behind to allow
+*	the use to modify their local resources.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace AssignmentManager.CodeFiles
+{
+    /// <summary>
+    /// Interaction logic for ModifyLocalResources.xaml
+    /// </summary>
+    public partial class ModifyLocalResources : Window
+    {
+        public ModifyLocalResources()
+        {
+            InitializeComponent();
+        }
+    }
+}
