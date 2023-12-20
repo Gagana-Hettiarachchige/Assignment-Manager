984c9a93d019c2390caba61ca7c3f400b18eaf0e
Gagana Hettiarachchige <81533753+Gagana-Hettiarachchige@users.noreply.github.com>
2023-12-19 19:26:47 -0500

    Added file header comment.

---
+++ ResourceOpenMenu.xaml.cs
@@ -0,0 +1,38 @@
+﻿/* 
+* FILE          : ResourceOpenMenu.xaml.cs
+* PROJECT       : Assignment Manager
+* PROGRAMMER    : Gagana Hettiarachchige
+* FIRST VERSION : 2023-12-19
+* DESCRIPTION   :
+*	This file holds the code behind for the
+*	menu that allows the user to open
+*	certain or all resources.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace AssignmentManager.CodeFiles
+{
+    /// <summary>
+    /// Interaction logic for ResourceOpenMenu.xaml
+    /// </summary>
+    public partial class ResourceOpenMenu : Window
+    {
+        public ResourceOpenMenu(List<string> resources)
+        {
+            InitializeComponent();
+        }
+    }
+}
