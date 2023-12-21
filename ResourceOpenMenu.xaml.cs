2094d84195b82e472869c495b36d42676b83f411
Gagana Hettiarachchige <81533753+Gagana-Hettiarachchige@users.noreply.github.com>
2023-12-21 15:51:28 -0500

    Created logic to open, open all, and open cmd with command for resources.

--- ResourceOpenMenu.xaml.cs
+++ ResourceOpenMenu.xaml.cs
@@ -9,8 +9,10 @@
 *	certain or all resources.
 */
 
+using AssignmentManager.Properties;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -22,6 +24,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.IO;
 
 namespace AssignmentManager.CodeFiles
 {
@@ -47,23 +50,83 @@ namespace AssignmentManager.CodeFiles
         public ResourceOpenMenu(List<string> resources)
         {
             InitializeComponent();
-            Title = "Open Resources";
-            /* Need to load resources into a control. */
+
+            /* Putting the resources into the listbox. */
+            ResourcesList.ItemsSource = resources;
         }
 
         private void OpenAllOption_Click(object sender, RoutedEventArgs e)
         {
-
+            /* Looping through each resource. */
+            foreach (string resource in ResourcesList.Items)
+            {
+                /* Opening all of them. */
+                Process.Start('"' + resource + '"');
+            }
         }
 
         private void OpenOption_Click(object sender, RoutedEventArgs e)
         {
-
+            /* Making sure something is selected. */
+            if (ResourcesList.SelectedItem != null)
+            {
+                /* Opening the selected resource. */
+                Process.Start('"' + ResourcesList.SelectedItem.ToString() + '"');
+            }
         }
 
         private void OpenCmdOption_Click(object sender, RoutedEventArgs e)
         {
+            
 
+            /* Checking if something is selected. */
+            if (ResourcesList.SelectedItem != null)
+            {
+
+                string resource = ResourcesList.SelectedItem.ToString();
+                Uri online_resource;
+                ProcessStartInfo info = new ProcessStartInfo();
+
+                Uri.TryCreate(resource, UriKind.Absolute, out online_resource);
+
+                /* Checking if an online resource. */
+                if (online_resource.Scheme == Uri.UriSchemeHttp || 
+                    online_resource.Scheme == Uri.UriSchemeHttps)
+                {
+                    /* Pinging the domain if online website. */
+                    info.WorkingDirectory = @"C:\";
+                    info.FileName = "CMD.exe";
+                    info.Arguments = "/K ping " + online_resource.Host;
+                    Process.Start(info);
+                }
+
+                else
+                {
+                    FileAttributes resource_attributes = File.GetAttributes(resource);
+
+                    /* Checking if it was a directory or File. */
+                    if (resource_attributes == FileAttributes.Directory)
+                    {
+                        /* Running git status on directory. */
+                        info.WorkingDirectory = resource;
+                        info.FileName = "CMD.exe";
+                        info.Arguments = "/K git status";
+                        Process.Start(info);
+                    }
+                    else
+                    {
+                        string directory = System.IO.Path.GetDirectoryName(resource);
+
+                        /* Running git diff on file. */
+                        info.WorkingDirectory = directory;
+                        info.FileName = "CMD.exe";
+                        info.Arguments = "/K git diff " + resource;
+                        Process.Start(info);
+                    }
+                }
+                
+               
+            }
         }
     }
 }
