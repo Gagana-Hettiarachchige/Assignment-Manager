90e51617ff709552e300a7916e2849f726e58dc9
Gagana Hettiarachchige <81533753+Gagana-Hettiarachchige@users.noreply.github.com>
2023-12-22 09:50:19 -0500

    Added file header comment.

---
+++ InsertAlterDeleteMenu.xaml.cs
@@ -0,0 +1,39 @@
+﻿/* 
+* FILE          : InsertAlterDelete.xaml.cs
+* PROJECT       : Assignment Manager
+* PROGRAMMER    : Gagana Hettiarachchige
+* FIRST VERSION : 2023-12-22
+* DESCRIPTION   :
+*	This file holds the code behind of the insert,
+*	alter, delete menu and allows the user to 
+*	insert assignments, alter selected assignments, and
+*	delete assignments in the database.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace AssignmentManager.CodeFiles
+{
+    /// <summary>
+    /// Interaction logic for InsertAlterDeleteMenu.xaml
+    /// </summary>
+    public partial class InsertAlterDeleteMenu : Window
+    {
+        public InsertAlterDeleteMenu()
+        {
+            InitializeComponent();
+        }
+    }
+}
