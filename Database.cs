43697c7aa59b1cf52d0a012fe7318b34817ce0ef
Gagana Hettiarachchige <81533753+Gagana-Hettiarachchige@users.noreply.github.com>
2023-12-21 19:47:32 -0500

    Added collection to hold all the assignments in the database.

--- Database.cs
+++ Database.cs
@@ -9,15 +9,17 @@
 
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace AssignmentManager.CodeFiles
 {
-    internal class Database
+    internal static class Database
     {
-
+        /* All assignments in database. */
+        public static ObservableCollection<Assignment> databaseAssignments = new ObservableCollection<Assignment>();
 
     }
 }
