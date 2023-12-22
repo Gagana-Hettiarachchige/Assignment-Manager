2f553387a5bd2dfb5362f18130827994c64f945e
Gagana Hettiarachchige <81533753+Gagana-Hettiarachchige@users.noreply.github.com>
2023-12-21 19:41:10 -0500

    Added file header comment.

---
+++ Database.cs
@@ -0,0 +1,23 @@
+﻿/* 
+* FILE          : Database.cs
+* PROJECT       : Assignment Manager
+* PROGRAMMER    : Gagana Hettiarachchige
+* FIRST VERSION : 2023-12-21
+* DESCRIPTION   :
+*	This file holds the code to interact with the database.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssignmentManager.CodeFiles
+{
+    internal class Database
+    {
+
+
+    }
+}
