c7219145676fafb863582ca7e48477c06a8c01be
Gagana Hettiarachchige <81533753+Gagana-Hettiarachchige@users.noreply.github.com>
2023-12-22 14:37:25 -0500

    Added list and dictionary for reading the column names and column values.

--- Assignments.cs
+++ Assignments.cs
@@ -15,6 +15,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Controls;
+using System.Windows.Diagnostics;
 
 namespace AssignmentManager.CodeFiles
 {
@@ -25,53 +26,143 @@ namespace AssignmentManager.CodeFiles
         private string className = "ABCD";
         private string assignmentName = "A-01";
         private double assignmentWeight = 10.01;
-        private DateTime dueDate = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day, 23, 00, 00);
+        private DateTime dueDate = new DateTime();
         private string assignmentStatus = "";
         private string localResources = "";
         private string onlineResources = "";
 
+        /* Names of the rows for display purposes. */
+        public static readonly List<string> ColumnNames = new List<string>()
+        {
+            "Number",
+            "Class",
+            "Name",
+            "Weight",
+            "Due Date",
+            "Status",
+            "Local Resources",
+            "Online Resources"
+        };
+
+        /* Dictionary to iterate through values using the name. */
+        public readonly Dictionary<string, string> ColumnValues = new Dictionary<string, string>()
+        {
+            { ColumnNames[0], "0"},
+            { ColumnNames[1], "ABCD"},
+            { ColumnNames[2], "A-01" },
+            { ColumnNames[3], "10.01" },
+            { ColumnNames[4], "yyyy-MM-dd hh:mm:ss tt"},
+            { ColumnNames[5], "Incomplete" },
+            { ColumnNames[6], "" },
+            { ColumnNames[7], "" },
+        };
+
+
         /* Properties for assignment assignment values. */
+
         public int AssignmentNumber 
         { 
-            get { return assignmentNumber; } 
-            set { assignmentNumber = value; } 
-        }
-        public string ClassName 
-        {
-            get { return className; }
-            set { className = value; }
-        }
-        public string AssignmentName 
-        {
-            get { return assignmentName; }
-            set { assignmentName = value; }
-        }
-        public double AssignmentWeight 
-        {
-            get { return assignmentWeight; }
-            set { assignmentWeight = value; }
-        }
-        public DateTime DueDate 
-        {
-            get { return dueDate; }
-            set { dueDate = value; }
-        }
-        public string AssignmentStatus 
-        {
-            get { return assignmentStatus; }
-            set { assignmentStatus = value; }
-        }
-        public string LocalResources 
-        {
-            get { return localResources; }
-            set { localResources = value; }
-        }
-        public string OnlineResources 
-        {
-            get { return onlineResources; }
-            set { onlineResources = value; }
+            get 
+            { 
+                return assignmentNumber; 
+            } 
+            set
+            { 
+                assignmentNumber = value; 
+                ColumnValues[ColumnNames[0]] = value.ToString(); 
+            } 
         }
 
+        public string ClassName 
+        {
+            get 
+            { 
+                return className; 
+            }
+            set 
+            { 
+                className = value; 
+                ColumnValues[ColumnNames[1]] = value.ToString(); 
+            }
+        }
+
+        public string AssignmentName 
+        {
+            get 
+            { 
+                return assignmentName; 
+            }
+            set 
+            { 
+                assignmentName = value; 
+                ColumnValues[ColumnNames[2]] = value.ToString(); 
+            }
+        }
+
+        public double AssignmentWeight 
+        {
+            get 
+            { 
+                return assignmentWeight; 
+            }
+            set 
+            { 
+                assignmentWeight = value; 
+                ColumnValues[ColumnNames[3]] = value.ToString(); 
+            }
+        }
+
+        public DateTime DueDate 
+        {
+            get 
+            { 
+                return dueDate; 
+            }
+            set 
+            { 
+                dueDate = value; 
+                ColumnValues[ColumnNames[4]] = value.ToString(); 
+            }
+        }
+
+        public string AssignmentStatus 
+        {
+            get 
+            { 
+                return assignmentStatus; 
+            }
+            set 
+            { 
+                assignmentStatus = value; 
+                ColumnValues[ColumnNames[5]] = value.ToString(); 
+            }
+        }
+
+        public string LocalResources 
+        {
+            get 
+            { 
+                return localResources; 
+            }
+            set
+            { 
+                localResources = value; 
+                ColumnValues[ColumnNames[6]] = value.ToString(); 
+            }
+        }
+
+        public string OnlineResources 
+        {
+            get 
+            { 
+                return onlineResources; 
+            }
+            set 
+            { 
+                onlineResources = value; 
+                ColumnValues[ColumnNames[7]] = value.ToString(); 
+            }
+        }
 
 
     }
