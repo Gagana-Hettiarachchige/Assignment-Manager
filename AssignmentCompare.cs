2dcc8169b83127b96d9ff23debe2bd2ce33a9844
Gagana Hettiarachchige <81533753+Gagana-Hettiarachchige@users.noreply.github.com>
2023-12-22 11:28:52 -0500

    Added file header comment.

---
+++ AssignmentCompare.cs
@@ -0,0 +1,28 @@
+﻿/* 
+* FILE          : AssignmentCompare.cs
+* PROJECT       : Assignment Manager
+* PROGRAMMER    : Gagana Hettiarachchige
+* FIRST VERSION : 2023-12-22
+* DESCRIPTION   :
+*	This file holds the class defintion for 
+*	AssignmentCompare which is an object that holds
+*	column names, the old assignment's values 
+*	(the previous selected assignment values), and 
+*	the new assignment's values 
+*	(the selcted assignment's potential change or new
+*	assignment to add to the database).
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssignmentManager.CodeFiles
+{
+    internal class AssignmentCompare
+    {
+
+    }
+}
