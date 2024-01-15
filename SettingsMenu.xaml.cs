0f0919b954503d41b45f897b4d068842d47d11a6
Gagana Hettiarachchige <81533753+Gagana-Hettiarachchige@users.noreply.github.com>
2024-01-15 12:57:28 -0500

    Added call to create settings menu on button click.

---
+++ SettingsMenu.xaml.cs
@@ -0,0 +1,36 @@
+﻿/* 
+* FILE          : SettingsMenu.xaml.cs
+* PROJECT       : Assignment Manager
+* PROGRAMMER    : Gagana Hettiarachchige
+* FIRST VERSION : 2024-01-15
+* DESCRIPTION   :
+*	This file holds the logic for updating the UI and settings.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace AssignmentManager.CodeFiles
+{
+    /// <summary>
+    /// Interaction logic for SettingsMenu.xaml
+    /// </summary>
+    public partial class SettingsMenu : Window
+    {
+        public SettingsMenu()
+        {
+            InitializeComponent();
+        }
+    }
+}
