c2bc83e4031f561f71b8f7e1bd5d649f319f3b4b
Gagana Hettiarachchige <81533753+Gagana-Hettiarachchige@users.noreply.github.com>
2024-01-16 08:19:25 -0500

    Created logic for settings and started with animation toggle.

--- SettingsMenu.xaml.cs
+++ SettingsMenu.xaml.cs
@@ -9,6 +9,7 @@
 
 using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -31,7 +32,66 @@ namespace AssignmentManager.CodeFiles
         public SettingsMenu()
         {
             InitializeComponent();
+
+            /* Adding the settings categories. */
+            CategoriesListBox.Items.Add("Visual");
+            CategoriesListBox.Items.Add("Notifications");
+            CategoriesListBox.Items.Add("Key Binding");
+            CategoriesListBox.Items.Add("Database");
+
+            /* Setting default selected to first. */
+            CategoriesListBox.SelectedIndex = 0;
+
+            /* Getting settings. */
+            /* Loading in the visual settings as is. */
+            string current_setting = ConfigurationManager.AppSettings["animationsEnabled"];
+            if (current_setting == "true")
+            {
+                AnimationToggleButton.Content = "True";
+            }
+            else if (current_setting == "false") 
+            {
+                AnimationToggleButton.Content = "False";
+            }
         }
 
+        private void CategoriesListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (CategoriesListBox.SelectedItem != null) 
+            {
+
+                /* Checking if visual was selected. */
+                if (CategoriesListBox.SelectedItem.ToString() == "Visual")
+                {
+                    /* Making the visual settings visible. */
+                    VisualSettings.Visibility = Visibility.Visible;
+                }
+                else
+                {
+                    VisualSettings.Visibility = Visibility.Hidden;
+                }
+            }
+        }
+
+        private void AnimationToggleButton_Click(object sender, RoutedEventArgs e)
+        {
+            /* Switching the button. */
+            if (AnimationToggleButton.Content.ToString() == "True")
+            {
+                AnimationToggleButton.Content = "False";
+            }
+
+            else if (AnimationToggleButton.Content.ToString() == "False")
+            {
+                AnimationToggleButton.Content = "True";
+            }
+
+            /* Updating settings. */
+            Configuration settings = ConfigurationManager.OpenExeConfiguration
+                                     (ConfigurationUserLevel.None);
+            settings.AppSettings.Settings["animationsEnabled"].Value = AnimationToggleButton.Content.ToString().ToLower();
+            settings.Save(ConfigurationSaveMode.Full);
+            ConfigurationManager.RefreshSection("appSettings");
+        }
     }
 }
