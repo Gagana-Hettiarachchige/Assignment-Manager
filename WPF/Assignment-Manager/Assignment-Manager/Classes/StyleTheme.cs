296d204ef337b9c455795ad07b6f9ef3048ac7a6
Gagana Hettiarachchige <81533753+Gagana-Hettiarachchige@users.noreply.github.com>
2025-03-13 12:37:26 -0400

    Started creating class for styling the app.

---
+++ WPF/Assignment-Manager/Assignment-Manager/Classes/StyleTheme.cs
@@ -0,0 +1,23 @@
+﻿/* 
+* FILE          : StyleTheme.cs
+* PROJECT       : Assignment Manager
+* PROGRAMMER    : Gagana "Gage" Hettiarachchige
+* FIRST VERSION : 2025-03-13
+* DESCRIPTION   :
+*	This file holds the class definition
+*	for styling the app.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_Manager.Classes
+{
+
+    class StyleTheme
+    {
+    }
+}
